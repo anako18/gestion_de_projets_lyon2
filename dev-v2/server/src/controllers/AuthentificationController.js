96c1aa67751b304c3ae514386cd5e23b72389b09
gcch <contact@gcch.fr>
2021-11-23 11:40:22 +0100

    Suppression des anciennes versions

--- dev-v2/server/src/controllers/AuthentificationController.js
+++
@@ -1,9 +0,0 @@
-// const { User } = require('../model')
-
-module.exports = {
-  register (req, res) {
-    res.send({
-      message: `Utilisateur enregistré avec l'adresse ${req.body.email}.`
-    })
-  }
-}
--- dev-v2/server/src/routes.js
+++
@@ -1,6 +0,0 @@
-const AuthentificationController = require('./controllers/AuthentificationController')
-
-module.exports = (app) => {
-  app.post('/register',
-    AuthentificationController.register)
-}
