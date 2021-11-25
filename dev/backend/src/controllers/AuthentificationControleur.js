f602d6a1465896eafb26817ff19cb6c7d4bd32ae
jbob-zdar <g.colin-haag@univ-lyon2.fr>
2021-11-25 16:22:20 +0100

    Actualisation du nom de la racine du projet

---
+++ dev/backend/src/controllers/AuthentificationControleur.js
@@ -0,0 +1,14 @@
+const { Utilisateur } = require('../models')
+
+module.exports = {
+  async authentification (req, res) {
+    try {
+      const utilisateur = await Utilisateur.create(req.body)
+      res.send(utilisateur.toJSON())
+    } catch (erreur) {
+      res.status(400).send({
+        erreur: 'Cette adresse email est déjà utilisée.'
+      })
+    }
+  }
+}
---
+++ dev/backend/src/policies/AuthentificationControleurPolitique.js
@@ -0,0 +1,38 @@
+const Joi = require('joi')
+
+module.exports = {
+  authentification (req, res, next) {
+    const schema = Joi.object({
+      email: Joi.string()
+        .email()
+        .min(5)
+        .max(64)
+        .required(),
+      password: Joi.string()
+        .regex(/(.){4,64}/)
+        .required()
+    })
+    const { erreur } = schema.validate(req.body)
+
+    if (erreur) {
+      switch (erreur.details[0].context.key) {
+        case 'email':
+          res.status(400).send({
+            erreur: 'Vous devez fournir une adresse email valide'
+          })
+          break
+        case 'password':
+          res.status(400).send({
+            erreur: 'Vous devez fournir un mot de passe entre 8 et 64 caractères.'
+          })
+          break
+        default:
+          res.status(400).send({
+            erreur: "Erreur lors de l'authentification."
+          })
+      }
+    } else {
+      next()
+    }
+  }
+}
