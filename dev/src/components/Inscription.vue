c78f88ff441fab5fec0cad5c4f361a0877475881
jbob-zdar <g.colin-haag@univ-lyon2.fr>
2021-11-25 16:21:45 +0100

    form completion
    feedback ?

--- dev/src/components/Inscription.vue
+++ dev/src/components/Inscription.vue
@@ -5,18 +5,22 @@
     <fieldset>
       <legend>Inscription</legend>
       <div class="groupe-champ">
-        <label for="email">Email</label>
-        <input type="email" name="Email" id="inscription-email">
-        <label for="utilisateur">Utilisateur</label>
-        <input type="text" name="Utilisateur" id="inscription-utilisateur">
-      </div>
-      <div class="groupe-champ">
         <label for="nom">Nom</label>
         <input type="text" name="Nom" id="inscription-nom">
         <label for="prenom">Prénom</label>
         <input type="text" name="Nom" id="inscription-prenom">
+        <label for="bdate">Date de naissance</label>
+        <input type="datetime" name="DateNaiss" id="inscription-dnaiss" placeholder="JJ/MM/AAAA">
+        <label for="email">Email</label>
+        <input type="email" name="Email" id="inscription-email">
+        <label for="utilisateur">Pseudo</label>
+        <input type="text" name="Utilisateur" id="inscription-utilisateur">
+      </div>
+      <div id="validation">
+        <button type="submit" class="validate-button">C'est parti !</button>
       </div>
     </fieldset>
+
     </form>
   </div>
 </template>
@@ -33,6 +37,7 @@ export default {
 </script>
 
 <style scoped>
+
 h1,
 h2 {
   font-weight: normal;
@@ -44,6 +49,8 @@ form {
 }
 fieldset {
   padding: 1rem;
+  border-radius: 25px;
+  width: 100%;
 }
 .groupe-champ {
   padding: 0 1rem;
@@ -64,5 +71,24 @@ legend {
   background-color: #000;
   color: #fff;
   padding: 3px 6px;
+}
+.validate-button{
+  background-color: black;
+  color: white;
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 0.5vw;
+  font-size: 1.2rem;
+  cursor: pointer;
+}
+.validate-button:hover{
+  color: black;
+  background-color: white;
+}
+#validation{
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
