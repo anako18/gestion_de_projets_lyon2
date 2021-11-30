c0abbc63c6db705c5ef85dffa1444b9b81e5e8c7
gcch <contact@gcch.fr>
2021-11-30 22:52:37 +0100

    Backend : Authentification basique
    L'authentification est désormais prise en charge, mais elle est basique.
    Il manque une gestion des erreurs proprement dite, une politique de validation (via Joi),
    et de meilleurs messages renvoyés à l'utilisateur.
    
    Sans parler qu'il n'y a pas encore d'intégration de JWT.

--- dev/backend/src/controllers/AuthentificationControleur.js
+++ dev/backend/src/controllers/AuthentificationControleur.js
@@ -1,14 +1,38 @@
-const { Utilisateur } = require('../models')
+const { Utilisateur } = require("../models/Utilisateur");
 
+// TODO: Gestion des erreurs pour l'authentification
 module.exports = {
   async authentification (req, res) {
     try {
-      const utilisateur = await Utilisateur.create(req.body)
-      res.send(utilisateur.toJSON())
+      const { email, password } = req.body;
+      const utilisateur = await Utilisateur.findOne({
+        where: {
+          email: email
+        }
+      });
+
+      // TODO: Passer la validation des données d'authentification dans un fichier de politique
+      if (!utilisateur) {
+        return res.status(403).send({
+          erreur: "Les informations de connexion sont incorrectes."
+        });
+      }
+      const estMotDePasseValide = password === utilisateur.password;
+      if (!estMotDePasseValide) {
+        return res.status(403).send({
+          erreur: "Les informations de connexion sont incorrectes."
+        });
+      }
+
+      // TODO: Réfléchir à un meilleur message de succès
+      const utilisateurJson = utilisateur.toJSON();
+      res.send({
+        succes: "Utilisateur authentifié ! Bienvenue " + utilisateurJson.email + " !"
+      });
     } catch (erreur) {
-      res.status(400).send({
-        erreur: 'Cette adresse email est déjà utilisée.'
-      })
+      res.status(500).send({
+        erreur: "Erreur durant l'authentification."
+      });
     }
   }
-}
+};
