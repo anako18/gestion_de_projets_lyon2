36a3ede112f3eb30f89c91a89258a64771a2b58c
gcch <contact@gcch.fr>
2021-11-30 22:54:34 +0100

    Backend : Réécriture de la politique d'enregistrement
    Le fichier de politique pour l'enregistrement a été réécrit pour plus de clarté,
    avec des JSDocs, une meilleur gestion des messages d'erreur.

--- dev/backend/src/policies/EnregistrementControleurPolitique.js
+++ dev/backend/src/policies/EnregistrementControleurPolitique.js
@@ -1,6 +1,12 @@
-const Joi = require('joi')
+const Joi = require("joi");
 
 module.exports = {
+  /**
+   * Définit le schéma de l'enregistrement d'un utilisateur à valider par Joi.
+   * @param {*} req La requête qui sera validée par Joi.
+   * @param {*} res La réponse retournée par Joi.
+   * @param {*} next Le passage de la requête à l'élément suivant, ici le contrôleur.
+   */
   enregistrement (req, res, next) {
     const schema = Joi.object({
       email: Joi.string()
@@ -9,25 +15,61 @@ module.exports = {
         .required()
         .email(),
       password: Joi.string()
-        // .regex(/(.){4,64}/)
         .min(6)
         .max(64)
         .required()
-    })
+    });
 
-    const resultat = schema.validate(req.body)
+    /** Enclenche la validation par Joi. */
+    const resultat = schema.validate(req.body);
+
+    // TODO: Créer une fonction qui fonctionne quelque soit le nom du champ
+    /**
+     * Sélectionne le message d'erreur qui sera envoyé à l'utilisateur.
+     * @param {*} champ Le champ avant provoqué l'erreur.
+     * @param {*} type Le type d'erreur renvoyé par Joi.
+     * @returns Un message d'erreur pour l'utilisateur.
+     */
+    const selectionMessageErreur = (champ, type) => {
+      if (champ === "email") {
+        switch (type) {
+          case "string.empty":
+            return "Le champ de l'email ne peut être vide.";
+          case "string.min":
+            return "Le champ de l'email doit au moins contenir 5 caractères.";
+          case "string.max":
+            return "Le champ de l'email doit au plus contenir 64 caractères.";
+          default:
+            return "Email : erreur inconnue.";
+        }
+      } else {
+        switch (type) {
+          case "string.empty":
+            return "Le champ du mot de passe ne peut être vide.";
+          case "string.min":
+            return "Le champ du mot de passe doit au moins contenir 5 caractères.";
+          case "string.max":
+            return "Le champ du mot de passe doit au plus contenir 64 caractères.";
+          default:
+            return "Mot de passe : erreur inconnue.";
+        }
+      }
+    };
 
     if (resultat.error) {
+      const erreurJson = resultat.error.details[0];
+      const erreurMessage = selectionMessageErreur(erreurJson.path[0], erreurJson.type);
+
       res.status(400).json({
-        statut: 'Échec',
-        resultat: resultat
-      })
+        statut: "Échec",
+        message: erreurMessage
+        // erreur: erreurJson
+      });
     } else {
       res.status(200).json({
-        statut: 'Succès',
-        resultat: resultat
-      })
-      next()
+        statut: "Succès"
+      });
+      next();
     }
   }
-}
+};
