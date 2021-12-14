66fe5b485b69c266a4a4883fa8619820dc941b10
jbob-zdar <g.colin-haag@univ-lyon2.fr>
2021-12-14 12:58:00 +0100

    Front : Création vue Inscription

---
+++ dev/frontend/src/views/Inscription.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <main>
+    <EnregistrementFormulaire />
+  </main>
+</template>
+
+<script>
+import EnregistrementFormulaire from "../modules/CompteModule/EnregistrementFormulaire.vue";
+export default {
+  name: "InscriptionVue",
+  components: {
+    EnregistrementFormulaire
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  @import "../styles/common.scss";
+  @import url('https://fonts.googleapis.com/css2?family=Hind:wght@300;400;500;700&family=Roboto:wght@300;400;500;700&display=swap');
+</style>
