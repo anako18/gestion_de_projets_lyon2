628b6936c2ec0f8b3ca3593ec8348d27f84be6a1
jbob-zdar <g.colin-haag@univ-lyon2.fr>
2021-12-26 14:46:23 +0100

    Front : MÀJ de l'outillage
    EditorConfig, Stylelint

---
+++ dev/frontend/stylelint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard-scss",
+    "stylelint-config-recommended-vue"
+  ],
+  rules: {
+    "selector-pseudo-element-no-unknown": [
+      true,
+      {
+        ignorePseudoElements: ["v-deep"]
+      }
+    ]
+  }
+};
