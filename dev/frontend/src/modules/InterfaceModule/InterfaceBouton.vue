e582b1d205a8091de23a2fa46660477436e1347b
jbob-zdar <g.colin-haag@univ-lyon2.fr>
2021-12-28 15:32:52 +0100

    Front : composant Bouton

---
+++ dev/frontend/src/modules/InterfaceModule/InterfaceBouton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button
+    type="button"
+    :class="style"
+  >
+    {{ valeur }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    valeur: {
+      type: String,
+      default: "Texte",
+      require: true
+    }
+  },
+  data () {
+    const correspondanceType = () => {
+      if (this.type === "connexion") {
+        return "bouton-style-1 couleur-c2 icone-connexion"
+      }
+      if (this.type === "inscription") {
+        return "bouton-style-1 couleur-c1 icone-inscription"
+      }
+    }
+    return {
+      style: correspondanceType()
+    }
+  }
+}
+</script>
