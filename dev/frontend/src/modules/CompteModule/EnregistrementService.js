0158617196110394e5fc7b465ec4ba0bf8372b24
jbob-zdar <g.colin-haag@univ-lyon2.fr>
2021-12-29 15:51:22 +0100

    Front : Validation des données
    Le frontend est désormais capable de valider les données renseignées par l'utilisateur à
    l'enregistrement et la connexion.

--- dev/frontend/src/modules/CompteModule/EnregistrementService.js
+++ dev/frontend/src/modules/CompteModule/EnregistrementService.js
@@ -1,12 +1,26 @@
 "use strict"
 import Api from "@m/ApiModule/ApiService"
+import CompteValidation from "@m/CompteModule/CompteValidation"
 
 /**
- * Requête d'enregistrement
- * @param credentials : Les identifiants utilisés pour s'enregistrer
+ * Valide les identifiants renseignés par l'utilisateur pour l'enregistrement.
+ * @param {Object} identifiants Identifiants renseignés par l'utilisateur.
+ * @returns Vrai si les vadiations passent, une ou plusieurs erreurs sinon.
  */
+const validation = function (identifiants) {
+  return CompteValidation(identifiants)
+}
+
+/**
+ * Envoie la requête d'enregistrement
+ * @param {Object} identifiants Les identifiants utilisés pour s'enregistrer
+ * @returns Un nouvel utilisateur si la requête succède, une erreur sinon.
+ */
+const enregistrement = function (identifiants) {
+  return Api().post("enregistrement", identifiants)
+}
+
 export default {
-  enregistrement (credentials) {
-    return Api().post("enregistrement", credentials)
-  }
+  validation,
+  enregistrement
 }
