ffd071fed19209281a49bb97d12183af8cb992af
Anastasia <nuka97@yandex.ru>
2022-01-03 00:28:20 +0300

    ajouter logo et filters components

---
+++ dev/frontend/src/views/EvenementsList.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="evenements-liste">
+    <header class="header-logo">
+        <img src="../assets/logo.png">  
+    </header>
+    <FiltersComponents/>
+  </main>
+</template>
+
+<script>
+import FiltersComponents from "../modules/EvenementsModule/Filters.vue";
+export default {
+  name: "Filters",
+  components: {
+    FiltersComponents
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
