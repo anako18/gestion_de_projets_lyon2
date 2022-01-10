098e4f2ccc3f634c33f484a336e04912b90afe1a
Anastasia <nuka97@yandex.ru>
2022-01-10 20:08:03 +0300

    changer coulers de footer panel

--- dev/frontend/src/modules/Footer.vue
+++ dev/frontend/src/modules/Footer.vue
@@ -2,44 +2,73 @@
   <div class="panel-footer">
     <div class="panel-element">
       <div class="recherche-image-panel"></div>
-      <a href="/evenements-liste">
-        <img class="panel-image" src="../../src/assets/panel/search-logo.png" />
+      <a class="link" href="/evenements-liste">
+        <img
+          class="panel-image"
+          src="../../src/assets/panel/active-search-logo.png"
+        />
         <span class="caption">Recherche</span>
       </a>
     </div>
     <div class="panel-element">
       <div class="recherche-image-panel"></div>
-      <a href="/favoris-liste">
-        <img class="panel-image" src="../../src/assets/panel/heart-logo.png" />
+      <a class="link" href="/favoris-liste">
+        <img
+          class="panel-image"
+          src="../../src/assets/panel/active-heart-logo.png"
+        />
         <span class="caption">Favoris</span>
       </a>
     </div>
     <div class="panel-element">
       <div class="recherche-image-panel"></div>
-      <a href="/mes-evenements">
+      <a  class="link" href="/mes-evenements">
         <img
           class="panel-image"
-          src="../../src/assets/panel/calender-logo.png"
+          src="../../src/assets/panel/active-calender-logo.png"
         />
         <span class="caption">Evenements</span>
       </a>
     </div>
     <div class="panel-element">
       <div class="recherche-image-panel"></div>
-      <a href="#">
-        <img class="panel-image" src="../../src/assets/panel/toc-logo.png" />
+      <a  class="link" href="#">
+        <img
+          class="panel-image"
+          src="../../src/assets/panel/active-toc-logo.png"
+        />
         <span class="caption">J'organise</span>
       </a>
     </div>
     <div class="panel-element">
       <div class="recherche-image-panel"></div>
-      <a href="#">
+      <a  class="link" href="#">
         <img
           class="panel-image"
-          src="../../src/assets/panel/profile-logo.png"
+          src="../../src/assets/panel/active-profile-logo.png"
         />
         <span class="caption">Mon compte</span>
       </a>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    setActive() {
+      var i, tablinks;
+      tablinks = document.getElementsByClassName('link');
+      for (i = 0; i < tablinks.length; i++) {
+        console.log(tablinks[i].href);
+        if (tablinks[i].href === window.location.href) {
+          tablinks[i].className+= " activelink";
+        }
+      }
+    },
+  },
+  mounted() {
+    this.setActive()
+  }
+};
+</script>
