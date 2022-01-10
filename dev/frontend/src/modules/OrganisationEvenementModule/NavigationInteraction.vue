603dacd431748f1ec94558ce90a164d2a8c16b13
Gauthier COLIN--HAAG <contact@gcch.fr>
2022-01-10 20:39:59 +0100

    Front : Création évène. en cours

---
+++ dev/frontend/src/modules/OrganisationEvenementModule/NavigationInteraction.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navigation">
+    <img
+      class="navigation__image"
+      src="@ai/icone--validation--nop.svg"
+      alt="Icône de la navigation"
+    >
+    <span class="navigation__texte">{{ texte }}</span>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    texte: {
+      type: String,
+      required: true,
+      default: "Navigation"
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "@s/organisation-evenements/nav-filariane";
+</style>
