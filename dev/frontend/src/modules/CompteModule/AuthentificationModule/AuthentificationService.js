c7e00c8aa6c80ed33e03377958a7ad30a4644bd2
Gauthier COLIN--HAAG <contact@gcch.fr>
2022-01-10 20:35:24 +0100

    Front : Commentaires dans Authentification

--- dev/frontend/src/modules/CompteModule/AuthentificationModule/AuthentificationService.js
+++ dev/frontend/src/modules/CompteModule/AuthentificationModule/AuthentificationService.js
@@ -1,11 +1,11 @@
 "use strict"
 import Api from "@m/ApiModule/ApiService.js"
 
-/**
- * Requête d'authentification
- * @param credentials Les identifiants utilisés pour s'authentifier
- */
 export default {
+  /**
+   * Requête d'authentification
+   * @param {Object} credentials Les identifiants utilisés pour s'authentifier
+   */
   authentification (credentials) {
     return Api().post("authentification", credentials)
   }
