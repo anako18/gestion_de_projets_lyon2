6972a2d864282bccc5fd36453dfc3b0b25054719
Anastasia <nuka97@yandex.ru>
2022-01-10 21:59:29 +0300

    ajouter bienvenue ecrans

---
+++ dev/frontend/src/views/Bienvenue.vue
@@ -0,0 +1,33 @@
+<template>
+  <main class="bienvenue">
+    <section>
+      <router-link to="/instructions">
+      <div style="text-align:center; padding: 50px 50px 20px 50px;">
+          <img
+        width="13%"
+          src='../assets/icon.png'
+        >
+      </div>
+      <div style="padding: 20px;">
+      <span class="bienvenue">
+        Bienvenue chez
+        <img
+        width="80%"
+          src='../assets/images/logo-white.png'
+          style="margin-left:40px;"
+        >
+      </span>
+      </div>
+          <img
+          src='../assets/images/illustration-1.png'
+          style="position:absolute;right:95px;bottom:-10px">
+      </router-link>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "BienvenueVue"
+}
+</script>
---
+++ dev/frontend/src/views/Instructions-3.vue
@@ -0,0 +1,30 @@
+<template>
+  <main class="inscructions">
+    <section>
+      <div style="text-align: center; padding: 70px 50px 20px 50px;  border-radius: 50%;overflow: hidden;">
+        <img src="../assets/images/inst3.png" width="65%" height="65%" />
+      </div>
+      <div class="titre-f">
+        Aujourd’hui <span style="color: #ea553d">hôte</span> 
+        et demain <span style="color: #ea553d">invité </span> ?
+      </div>
+      <div class="soustitre">
+        C’est toi qui décide ! Invites ou reçois tes convives pour partager un bon moment.
+      </div>
+      <div class="underlines">
+        <hr class="Rounded" />
+        <hr class="Rounded" />
+        <hr class="Rounded active" />
+      </div>
+      <div class="bouton">
+      <button class="yellow-button">C'est parti !</button>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "InstructionsVue",
+};
+</script>
