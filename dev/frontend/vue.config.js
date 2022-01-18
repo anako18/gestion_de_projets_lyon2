1ce05770905d84ab64f75f9f32aa0ad684128879
Gauthier COLIN--HAAG <contact@gcch.fr>
2022-01-18 15:03:43 +0100

    Front : Routes

--- dev/frontend/vue.config.js
+++ dev/frontend/vue.config.js
@@ -2,7 +2,7 @@ const path = require("path")
 
 module.exports = {
   devServer: {
-    // proxy: "http://localhost:8082/"
+    proxy: "http://localhost:8082/"
   },
   configureWebpack: {
     devServer: {
