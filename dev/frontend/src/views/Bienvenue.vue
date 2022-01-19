55bdf2f4c18555e14781aa88413418b13ff439c4
Anastasia <nuka97@yandex.ru>
2022-01-19 03:00:22 +0300

    one more fix

--- dev/frontend/src/views/Bienvenue.vue
+++ dev/frontend/src/views/Bienvenue.vue
@@ -1,34 +1,31 @@
 <template>
-  <main class="bienvenue">
-    <section>
-      <router-link to="/instructions1">
-        <div style=" padding: 50px 50px 20px;text-align: center;">
-          <img
-            width="13%"
-            src="../assets/icon.png"
-          >
+  <router-link to="/instructions1">
+    <main class="bienvenue">
+      <section>
+        <div style="padding: 50px 50px 20px; text-align: center">
+          <img width="13%" src="../assets/icon.png" />
         </div>
-        <div style="padding: 20px;">
+        <div style="padding: 20px">
           <span class="bienvenue">
             Bienvenue chez
             <img
               width="80%"
               src="../assets/images/logo-white.png"
-              style="margin-left: 40px;"
-            >
+              style="margin-left: 40px"
+            />
           </span>
         </div>
         <img
           src="../assets/images/illustration-1.png"
-          style="position: absolute;right: 95px;bottom: -10px;"
-        >
-      </router-link>
-    </section>
-  </main>
+          style="position: absolute; right: 95px; bottom: -10px"
+        />
+      </section>
+    </main>
+  </router-link>
 </template>
 
 <script>
 export default {
-  name: "BienvenueVue"
-}
+  name: "BienvenueVue",
+};
 </script>
