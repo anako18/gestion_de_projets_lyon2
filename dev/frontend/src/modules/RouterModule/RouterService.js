490f065d198da911f4767ac1ede4c091bff25e43
jbob-zdar <g.colin-haag@univ-lyon2.fr>
2022-01-21 18:58:10 +0100

    Front : Identification dans Router

--- dev/frontend/src/modules/RouterModule/RouterService.js
+++ dev/frontend/src/modules/RouterModule/RouterService.js
@@ -60,52 +60,82 @@ const routes = [
         name: "Type de cuisine",
         component: ModalInterface
       }
-    ]
+    ],
+    meta: {
+      requiertAuthentification: true
+    }
   },
   {
     path: "/evenements-liste",
     name: "Evenements",
-    component: EvenementsListVue
+    component: EvenementsListVue,
+    meta: {
+      requiertAuthentification: true
+    }
   },
   {
     path: "/favoris-liste",
     name: "Favoris",
-    component: FavorisListVue
+    component: FavorisListVue,
+    meta: {
+      requiertAuthentification: true
+    }
   },
   {
     path: "/mes-evenements",
     name: "MesEvenements",
-    component: MesEvenementsVue
+    component: MesEvenementsVue,
+    meta: {
+      requiertAuthentification: true
+    }
   },
   {
     path: "/page-evenement/:id",
     name: "PageEvenement",
-    component: PageEvenementVue
+    component: PageEvenementVue,
+    meta: {
+      requiertAuthentification: true
+    }
   },
   {
     path: "/bienvenue",
     name: "Bienvenue",
-    component: BienvenueVue
+    component: BienvenueVue,
+    meta: {
+      requiertAuthentification: true
+    }
   },
   {
     path: "/instructions1",
     name: "Instructions-1",
-    component: Instructions1Vue
+    component: Instructions1Vue,
+    meta: {
+      requiertAuthentification: true
+    }
   },
   {
     path: "/instructions2",
     name: "Instructions-2",
-    component: Instructions2Vue
+    component: Instructions2Vue,
+    meta: {
+      requiertAuthentification: true
+    }
   },
   {
     path: "/instructions3",
     name: "Instructions-3",
-    component: Instructions3Vue
+    component: Instructions3Vue,
+    meta: {
+      requiertAuthentification: true
+    }
   },
   {
     path: "/confirmation",
     name: "Confirmation",
-    component: ConfirmationVue
+    component: ConfirmationVue,
+    meta: {
+      requiertAuthentification: true
+    }
   }
 ]
 
@@ -113,6 +143,23 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiertAuthentification)) {
+    if (window.localStorage.getItem("idUtilisateur") === null) {
+      console.log("navigation: nop")
+      next({
+        path: "/connexion",
+        params: { nextUrl: to.fullPath }
+      })
+    } else {
+      console.log("navigation: ok")
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
